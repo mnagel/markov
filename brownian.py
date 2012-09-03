3100aee039a9b874e20b3d90f0551c1d93478e34
Michael Nagel <michael.nagel@devzero.de>
2012-09-03 23:38:38 +0200

    brownian motion example

---
+++ brownian.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+from markov import MarkovChain
+
+if len(sys.argv) != 2:
+	print "usage:"
+	print "python brownian.py $OUTLENGTH"
+	print "example:"
+	print "python brownian.py 300"
+	sys.exit(1)
+
+m = MarkovChain(1)
+
+n = 1000
+for i in xrange(-n, n):
+	m.observe_string("%d %d" % (i, i-1))
+	m.observe_string("%d %d" % (i, i+1))
+
+start = ["0"]
+
+#result = m.random_walk_string(int(sys.argv[1]), start)
+#print result
+
+from pylab import *
+result = m.random_walk(int(sys.argv[1]), start)
+result = [int(x) for x in result]
+plot(result)
+show()
