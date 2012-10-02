9618029113448b9ad109a5038471c73ba529e656
Michael Nagel <michael.nagel@devzero.de>
2012-10-02 22:43:36 +0200

    improve walkthrough

--- examples/walkthrough.py
+++ examples/walkthrough.py
@@ -3,23 +3,37 @@
 import sys
 from markov import MarkovChain
 
-order    = 2
-length   = 500
+def spacer():
+	print "-" * 60
+
+order    = 1
+length   = 50
 
 text = """
-	g'8 e'8 e'4 f'8 d'8 d'4
-	c'8 d'8 e'8 f'8 g'8 g'8 g'4
-	g'8 e'8 e'4 f'8 d'8 d'4
-	c'8 e'8 g'8 g'8 c'4 r4
-	g'8 e'8 e'4 f'8 d'8 d'4
-	c'8 d'8 e'8 f'8 g'8 g'8 g'4
-	g'8 e'8 e'4 f'8 d'8 d'4
-	c'8 e'8 g'8 g'8 c'4 r4
+	g8 e8 e4 f8 d8 d4
+	c8 d8 e8 f8 g8 g8 g4
+	g8 e8 e4 f8 d8 d4
+	c8 e8 g8 g8 c4 r4
+	g8 e8 e4 f8 d8 d4
+	c8 d8 e8 f8 g8 g8 g4
+	g8 e8 e4 f8 d8 d4
+	c8 e8 g8 g8 c4 r4
 """
 
 m = MarkovChain(order)
+
+print "observing %s" % text
+spacer()
 m.observe_string(text)
+print "TODO make logging verbose for observe()..."
+
+spacer()
+m.print_transitions()
+spacer()
 m.print_matrix()
+
 start = m.get_random_prestate()
+spacer()
 result = m.random_walk_string(length, start)
+
 print result
