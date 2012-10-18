0bb9be60b0b9002dd8a56b08f5cdf788a57d55e6
Michael Nagel <michael.nagel@devzero.de>
2012-10-18 09:57:15 +0200

    lily: quick and dirty script to check lilypond input

---
+++ examples/lily.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import random
+import re
+import string
+import subprocess
+import sys
+import os
+
+# from markov import MarkovChain
+
+"""
+if len(sys.argv) != 5:
+	print "usage:"
+	print "python algorithmic.music.py $ORDER $TEMPLATEFILE $DATAFILE $OUTLENGTH"
+	print "example:"
+	print "python algorithmic.music.py 1 data/lilypond-template.ly data/entchen.ly 300"
+	sys.exit(1)
+"""
+
+#order    = int(sys.argv[1])
+template = sys.argv[1]
+filename = sys.argv[2]
+#length   = int(sys.argv[4])
+
+
+lilypond = "lilypond"
+if sys.platform.startswith("darwin"):
+	lilypond = "/Applications/LilyPond.app/Contents/Resources/bin/lilypond"
+
+"""
+m = MarkovChain(order)
+m.observe_file(filename)
+start = m.get_random_prestate()
+result = m.random_walk_string(length, start)
+"""
+
+linestring = open(filename, "r").read()
+
+output = open(template, 'r').read()
+title = "plain %s" % (os.path.basename(filename))
+output = re.sub("%%%TITLE-GOES-HERE%%%", title, output)
+output = re.sub("%%%CONTENT-GOES-HERE%%%", linestring, output)
+
+rand = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(10))
+
+folder = os.path.dirname(template) + "/work/"
+lilypondfile = rand + ".ly"
+lilypondpath = folder + lilypondfile
+midipath = folder + rand + ".midi"
+pdfpath = folder + rand + ".pdf"
+
+with open(lilypondpath, "w+") as text_file:
+    text_file.write(output)
+
+print "lilypond input file created: %s" % lilypondpath
+
+cmd = ["bash", "-c", "cd %s && %s %s" % (folder, lilypond, lilypondfile)]
+print "running lilypond as: \n%s" % (" ".join(cmd))
+
+print "-" * 60
+subprocess.call(cmd)
+print "-" * 60
+
+print "lilypond finished, opening output files"
+
+subprocess.call(["xdg-open", pdfpath])
+subprocess.call(["xdg-open", midipath])
